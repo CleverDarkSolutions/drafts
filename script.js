da721c7f39cd14279df22054dd4da102fa26ef76
CleverDarkSolutions <66365110+CleverDarkSolutions@users.noreply.github.com>
2020-09-11 19:17:34 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,21 +4,36 @@ var pawnsArray = [];
 var pACount = 0;
 var ifFree1 = true;
 var ifFree2 = true;
-var ifEliminate1 = true;
-var ifEliminate2 = true;
+var ifEliminate1 = false;
+var ifEliminate2 = false;
 var ifSelected = false;
 var whoseTurn = "white";
+var potentialX1;
+var potentialY1;
+var potentialX2;
+var potentialY2;
+var eliminateX1;
+var eliminateY1;
+var eliminateX2;
+var eliminateY2;
+var toBeEliminatedX;
+var toBeEliminatedY;
+var comboX1;
+var comboY1;
+var comboX2;
+var comboY2;
 
 class Pawn {
-  constructor(id, posX, posY)
+  constructor(id, posX, posY, col)
   {
     this.id = id;
     this.posX = posX;
     this.posY = posY;
+    this.col = col;
   }
 }
 
-function changeTurns() // TO BE FIXED
+function changeTurns()
 {
   if(whoseTurn=="white")
   {
@@ -53,6 +68,27 @@ function loadCords() // building arrays of cords to use them later
   }
 }
 
+function eraseFromArray()
+{
+  for(var i=0;i<pawnsArray.length;i++)
+  {
+    if(toBeEliminatedX==pawnsArray[i].posX && toBeEliminatedY==pawnsArray[i].posY)
+    {
+      var id= pawnsArray[i].id;
+      document.getElementById(id).style.display="none";
+
+      pawnsArray[i].posX=0;
+      pawnsArray[i].posY=0;
+      pawnsArray[i].col="";
+      console.log("popoga");
+      toBeEliminatedX=0;
+      toBeEliminatedY=0;
+      break;
+    }
+  }
+}
+
+
 function changePlaces(fromX,fromY,toX,toY)
 {
   var id;
@@ -74,12 +110,16 @@ function changePlaces(fromX,fromY,toX,toY)
   match.posX = toX;
   match.posY = toY;
   document.getElementById('hover').innerHTML="";
-  changeTurns();
+  document.getElementById('eliminate').innerHTML="";
+    changeTurns();
 }
 
 function hoverMove(x1,y1,x2,y2)
 {
   var board = document.getElementById('hover');
+  board.innerHTML="";
+  $(".whitePawn").css("background","#663500");
+  $(".blackPawn").css("background","#663500");
   if(ifFree1==true)
   {
   board.innerHTML+="<div class='hoverMove' id='200'></div>";
@@ -98,116 +138,140 @@ function hoverMove(x1,y1,x2,y2)
 function hoverElimination(x1,y1,x2,y2)
 {
   var board = document.getElementById('eliminate');
+  board.innerHTML="";
+  $(".whitePawn").css("background","#663500");
+  $(".blackPawn").css("background","#663500");
   if(ifEliminate1==true)
-  {
-    board.innerHTML+="<div class='hoverMove' id='202'></div>";
-    $("#202").css("left",x1);
-    $("#202").css("top",y1);
-  }
-
-  if(ifEliminate2==true)
   {
     board.innerHTML+="<div class='hoverMove' id='203'></div>";
     $("#203").css("left",x1);
     $("#203").css("top",y1);
   }
+
+  if(ifEliminate2==true)
+  {
+    board.innerHTML+="<div class='hoverMove' id='204'></div>";
+    $("#204").css("left",x2);
+    $("#204").css("top",y2);
+  }
 }
 
 function playerMove(x,y)
 {
-  ifFree1=true;
-  ifFree2=true;
-  if(whoseTurn=="white")
+
+    ifFree1=true;
+    ifFree2=true;
+
+    for(var i=0;i<pawnsArray.length;i++)
+    {
+      if(potentialX1 <= 808 && potentialX1 >= 108 && potentialY1 > 0 && potentialY1 <= 808)
+      {
+        if(potentialX1 == pawnsArray[i].posX && potentialY1 == pawnsArray[i].posY)
+        {
+        ifFree1 = false;
+        if(pawnsArray[i].col!=whoseTurn)
+          ifEliminate1 = true;
+        }
+      }
+      else ifFree1= false;
+
+      if(potentialX2 <= 808 && potentialX2 > 0 && potentialY2 > 0 && potentialY2 <= 808)
+      {
+        if(potentialX2 == pawnsArray[i].posX && potentialY2 == pawnsArray[i].posY)
+        {
+        ifFree2 = false;
+        if(pawnsArray[i].col!=whoseTurn)
+          ifEliminate2 = true;
+        }
+      }
+      else ifFree2= false;
+    }
+
+    //ELIMINATION
+  if(ifFree1==false || ifFree2==false)
   {
-  var potentialX1 = x-100;
-  var potentialY1 = y-100;
-  var potentialX2 = x+100;
-  var potentialY2 = y-100;
-
-  var eliminateX1 = potentialX1 + 100;
-  var eliminateY1 = potentialY1 - 200;
-  var eliminateX2 = potentialX2 - 100;
-  var eliminateY2 = potentialY2 - 200;
-  }
-
-  if(whoseTurn=="black")
-  {
-  var potentialX1 = x-100;
-  var potentialY1 = y+100;
-  var potentialX2 = x+100;
-  var potentialY2 = y+100;
-
-  var eliminateX1 = potentialX1 + 100;
-  var eliminateY1 = potentialY1 + 200;
-  var eliminateX2 = potentialX2 - 100;
-  var eliminateY2 = potentialY2 + 200;
-  }
 
   for(var i=0;i<pawnsArray.length;i++)
   {
-    if(potentialX1 <= 808 && potentialX1 >= 108 && potentialY1 > 0 && potentialY1 <= 808)
-    {
-      if(potentialX1 == pawnsArray[i].posX && potentialY1 == pawnsArray[i].posY)
-      {
-      ifFree1 = false;
-      console.log(potentialX1+" "+potentialY1);
-      }
-    }
-    else ifFree1= false;
-
-    if(potentialX2 <= 808 && potentialX2 > 0 && potentialY2 > 0 && potentialY2 <= 808)
-    {
-      if(potentialX2 == pawnsArray[i].posX && potentialY2 == pawnsArray[i].posY)
-      {
-      ifFree2 = false;
-        console.log(potentialX2+" "+potentialY2);
-      }
-    }
-    else ifFree2= false;
-  }
-  //----------------------------------------ELIMINATION---------------------
-  for(var i=0;i<pawnsArray.length;i++)
-  {
+    if(whoseTurn=="black")
+      eliminateY1 = potentialY1 + 100;
+    if(whoseTurn=="white")
+      eliminateY1 = potentialY1 - 100;
+    if(x<potentialX1)
+      eliminateX1 = potentialX1 + 100;
+    if(x>potentialX1)
+      eliminateX1 = potentialX1 - 100;
     if(eliminateX1 <= 808 && eliminateX1 >= 108 && eliminateY1 > 0 && eliminateY1 <= 808)
     {
-      if(eliminateX1 == pawnsArray[i].posX && eliminateY1 == pawnsArray.posY)
+      if(eliminateX1 == pawnsArray[i].posX && eliminateY1 == pawnsArray[i].posY )
       {
-        ifEliminate1 = false;
-        console.log(eliminateX1+" "+eliminateY1);
+      ifEliminate1 = false;
+      console.log("zajete");
       }
     }
+    else ifEliminate1 = false;
 
-    if(eliminateX2 <= 808 && eliminateX2 >= 0 && eliminateY2 > 0 && eliminateY2 <= 808)
+    //ELIMINATION
+    if(whoseTurn=="black")
+      eliminateY2 = potentialY2 + 100;
+    if(whoseTurn=="white")
+      eliminateY2 = potentialY2 - 100;
+    if(x<potentialX2)
+      eliminateX2 = potentialX2 + 100;
+    if(x>potentialX2)
+      eliminateX2 = potentialX2 - 100;
+
+    if(eliminateX2 <= 808 && eliminateX2 >= 108 && eliminateY2 > 0 && eliminateY2 <= 808)
     {
-      if(eliminateX2 == pawnsArray[i].posX && eliminateY2 == pawnsArray.posY)
+      if(eliminateX2 == pawnsArray[i].posX && eliminateY2 == pawnsArray[i].posY )
       {
         ifEliminate2 = false;
-        console.log(eliminateX2+" "+eliminateY2);
       }
     }
+    else ifEliminate2 = false;
+  }
+
+  $(".whitePawn").css("background","#663500");
+  $(".blackPawn").css("background","#663500");
 
   }
 
-  //---------------------------------------------------------------------
- hoverMove(potentialX1,potentialY1,potentialX2,potentialY2);
- hoverElimination(eliminateX1,eliminateY1,eliminateX2,eliminateY2);
-if(ifFree1==true)
-{
- var hover1 = document.getElementById("200");
- hover1.addEventListener("click",function chPl(){ changePlaces(x,y,potentialX1,potentialY1) });
-}
-if(ifFree2==true)
-{
- var hover2 = document.getElementById("201");
- hover2.addEventListener("click",function chPl(){ changePlaces(x,y,potentialX2,potentialY2) });
-}
-if(ifFree1==false && ifFree2==false)
+   hoverMove(potentialX1,potentialY1,potentialX2,potentialY2);
+   hoverElimination(eliminateX1,eliminateY1,eliminateX2,eliminateY2);
+  if(ifFree1==true)
   {
-    $(".whitePawn").css("background","#663500");
-    $(".blackPawn").css("background","#663500");
-    document.getElementById("hover").innerHTML="";
-    return;
+   var hover1 = document.getElementById("200");
+   hover1.addEventListener("click",function chPl(){ changePlaces(x,y,potentialX1,potentialY1) });
   }
+  if(ifFree2==true)
+  {
+   var hover2 = document.getElementById("201");
+   hover2.addEventListener("click",function chPl(){ changePlaces(x,y,potentialX2,potentialY2) });
+  }
+
+  if(ifEliminate1==true)
+  {
+    var eliminateHover1 = document.getElementById("203");
+    eliminateHover1.addEventListener("click",function chPl(){
+       changePlaces(x,y,eliminateX1,eliminateY1);
+       toBeEliminatedX=potentialX1;
+       toBeEliminatedY=potentialY1;
+       eraseFromArray();
+     });
+  }
+
+  if(ifEliminate2==true)
+  {
+    var eliminateHover2 = document.getElementById("204");
+    eliminateHover2.addEventListener("click", function chPl(){
+      changePlaces(x,y,eliminateX2,eliminateY2);
+      toBeEliminatedX=potentialX2;
+      toBeEliminatedY=potentialY2;
+      eraseFromArray();
+    });
+  }
+
+
 }
 
 
@@ -222,7 +286,26 @@ function select(id) // choosing a pawn
   var currentPosX= pawnsArray[idx].posX;
   var currentPosY= pawnsArray[idx].posY;
 
-  playerMove(currentPosX,currentPosY,whoseTurn);
+  if(whoseTurn=="white")
+  {
+  potentialX1 = currentPosX-100;
+  potentialY1 = currentPosY-100;
+  potentialX2 = currentPosX+100;
+  potentialY2 = currentPosY-100;
+  }
+
+  if(whoseTurn=="black")
+  {
+  potentialX1 = currentPosX-100;
+  potentialY1 = currentPosY+100;
+  potentialX2 = currentPosX+100;
+  potentialY2 = currentPosY+100;
+  }
+
+  ifEliminate1=false;
+  ifEliminate2=false;
+
+  playerMove(currentPosX,currentPosY);
 }
 
 function addSelect(id) // adding event listener
@@ -277,15 +360,19 @@ function bindCords() // Binding the initial position of every pawn to its' objec
 
   }
 
+  var col;
+
 for(var j=1;j<=8;j++)
 {
+  if(j<4) col="black";
+  if(j>5) col="white";
   for(var i=1; i<=8;i++)
   {
     if(document.getElementById(i).className == "blackSquare" && j!=4 && j!=5 && j!=2 && j!=6 && j!=8)
     {
       var cX = getCords(i);
       var cY = getCords(j);
-      pawnsArray[pACount] = new Pawn (pawnsID[pACount],cX,cY);
+      pawnsArray[pACount] = new Pawn (pawnsID[pACount],cX,cY,col);
       pACount++;
     }
 
@@ -295,7 +382,7 @@ for(var j=1;j<=8;j++)
       {
         var cX = getCords(i+1);
         var cY = getCords(j);
-        pawnsArray[pACount] = new Pawn (pawnsID[pACount],cX,cY);
+        pawnsArray[pACount] = new Pawn (pawnsID[pACount],cX,cY,col);
         pACount++;
       }
     }
